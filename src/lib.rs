f5329e103f56f8099294989c423d66a9a150047e
Kirk Strauser <kirk@strauser.com>
2024-11-25 14:55:20 -0800

    Moved admin code into its own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
+pub mod admin;
 pub mod db;
+
+use time::{format_description::BorrowedFormatItem, macros::format_description};
+
+const TSTAMP_FORMAT: &[BorrowedFormatItem<'_>] =
+    format_description!("[year]-[month]-[day]@[hour]:[minute]:[second]");
+
+pub fn formatted_tstamp(tstamp: time::PrimitiveDateTime) -> String {
+    tstamp.format(&TSTAMP_FORMAT).unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
 use clap::{Parser, Subcommand};
-use diesel::prelude::*;
-use frozenbbs::db::{boards, establish_connection, posts, users};
-use time::{format_description::BorrowedFormatItem, macros::format_description};
-
-const TSTAMP_FORMAT: &[BorrowedFormatItem<'_>] =
-    format_description!("[year]-[month]-[day]@[hour]:[minute]:[second]");
-
-fn formatted_tstamp(tstamp: time::PrimitiveDateTime) -> String {
-    tstamp.format(&TSTAMP_FORMAT).unwrap()
-}
+use frozenbbs::{admin, db};
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None, arg_required_else_help = true)]
@@ -107,115 +98,36 @@ enum AdminPostCommands {
     },
 }
 
-fn user_list(connection: &mut SqliteConnection) {
-    println!(
-        "\
-# BBS users
-
-| Created at          | Last seen at        | Node ID   | Name | Long name                                |
-| ------------------- | ------------------- | --------- | ---- | ---------------------------------------- |"
-    );
-    for user in users::all(connection) {
-        println!(
-            "| {} | {} | {} | {:4} | {:40} |",
-            formatted_tstamp(user.created_at),
-            formatted_tstamp(user.last_seen_at),
-            user.node_id,
-            user.short_name,
-            user.long_name,
-        );
-    }
-}
-
-fn user_add(connection: &mut SqliteConnection, node_id: &str, short_name: &str, long_name: &str) {
-    let user = users::add(connection, node_id, short_name, long_name).unwrap();
-    println!("Created user #{}, '{}'", user.id, user.node_id);
-}
-
-fn board_list(connection: &mut SqliteConnection) {
-    println!(
-        "\
-# BBS boards
-
-| Created at          | Num | Name                           | Description |
-| ------------------- | --- | ------------------------------ | ----------- |"
-    );
-    for board in boards::all(connection) {
-        println!(
-            "| {} | {:3} | {:30} | {} |",
-            formatted_tstamp(board.created_at),
-            board.id,
-            board.name,
-            board.description,
-        );
-    }
-}
-
-fn board_add(connection: &mut SqliteConnection, name: &str, description: &str) {
-    let board = boards::add(connection, name, description).unwrap();
-    println!("Created board #{}, '{}'", board.id, board.name);
-}
-
-fn post_read(connection: &mut SqliteConnection, board_id: i32) {
-    let board = boards::get(connection, board_id).unwrap();
-    println!("# Posts in '{}'\n", board.name);
-
-    let post_info = posts::in_board(connection, board_id);
-    if post_info.is_empty() {
-        println!("There are no posts in board #{}", board_id);
-        return;
-    }
-
-    println!(
-        "\
-| Created at          | Node ID   | Body |
-| ------------------- | --------- | ---- |"
-    );
-
-    for (post, user) in post_info {
-        println!(
-            "| {} | {} | {} |", // "| {} | {:3} | {:30} | {} |",
-            formatted_tstamp(post.created_at),
-            user.node_id,
-            post.body,
-        );
-    }
-}
-
-fn post_add(connection: &mut SqliteConnection, board_id: i32, node_id: &str, content: &str) {
-    let user = users::get(connection, node_id).unwrap();
-    let post = posts::add(connection, user.id, board_id, content).unwrap();
-    println!("Created post #{}", post.id);
-}
-
 fn main() {
     let cli = Cli::parse();
 
-    let connection = &mut establish_connection();
+    let connection = &mut db::establish_connection();
 
     match &cli.command {
         Some(Subsystems::Admin { admin_command }) => match admin_command {
             Some(AdminCommands::User { user_command }) => match user_command {
-                Some(AdminUserCommands::List {}) => user_list(connection),
+                Some(AdminUserCommands::List {}) => admin::user_list(connection),
                 Some(AdminUserCommands::Add { id, short, long }) => {
-                    user_add(connection, id, short, long)
+                    admin::user_add(connection, id, short, long)
                 }
                 None => {}
             },
             Some(AdminCommands::Board { board_command }) => match board_command {
-                Some(AdminBoardCommands::List {}) => board_list(connection),
+                Some(AdminBoardCommands::List {}) => admin::board_list(connection),
                 Some(AdminBoardCommands::Add { name, description }) => {
-                    board_add(connection, name, description)
+                    admin::board_add(connection, name, description)
                 }
                 None => {}
             },
             Some(AdminCommands::Post { post_command }) => match post_command {
-                Some(AdminPostCommands::Read { board_id }) => post_read(connection, *board_id),
+                Some(AdminPostCommands::Read { board_id }) => {
+                    admin::post_read(connection, *board_id)
+                }
                 Some(AdminPostCommands::Add {
                     board_id,
                     node_id,
                     content,
-                }) => post_add(connection, *board_id, node_id, content),
+                }) => admin::post_add(connection, *board_id, node_id, content),
                 None => {}
             },
             None => {}
