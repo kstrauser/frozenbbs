0296d1619d76671976797dfda198fbae3746c251
Kirk Strauser <kirk@strauser.com>
2024-11-26 17:01:47 -0800

    Consistent spelling

--- src/admin.rs
+++ src/admin.rs
@@ -5,7 +5,7 @@ use diesel::SqliteConnection;
 // Today, for now, it's OK to fail when running user commands! A human will see the results,
 // including an explanatory traceback. It doesn't have to be pretty to be useful.
 
-pub fn user_list(connection: &mut SqliteConnection) {
+pub fn user_list(conn: &mut SqliteConnection) {
     println!(
         "\
 # BBS users
@@ -14,7 +14,7 @@ pub fn user_list(connection: &mut SqliteConnection) {
 | ------------------- | ------------------- | ---------- | ---- | ---------------------------------------- |"
     );
     let mut jackasses = false;
-    for user in users::all(connection) {
+    for user in users::all(conn) {
         println!(
             "| {} | {} | {}{} | {:4} | {:40} |",
             formatted_useconds(user.created_at_us),
@@ -35,27 +35,27 @@ pub fn user_list(connection: &mut SqliteConnection) {
 }
 
 pub fn user_add(
-    connection: &mut SqliteConnection,
+    conn: &mut SqliteConnection,
     node_id: &str,
     short_name: &str,
     long_name: &str,
     jackass: &bool,
 ) {
-    let user = users::add(connection, node_id, short_name, long_name, jackass).unwrap();
+    let user = users::add(conn, node_id, short_name, long_name, jackass).unwrap();
     println!("Created user #{}, '{}'", user.id, user.node_id);
 }
 
-pub fn user_ban(connection: &mut SqliteConnection, node_id: &str) {
-    let user = users::ban(connection, node_id).unwrap();
+pub fn user_ban(conn: &mut SqliteConnection, node_id: &str) {
+    let user = users::ban(conn, node_id).unwrap();
     println!("Banned user #{}, '{}'", user.id, user.node_id);
 }
 
-pub fn user_unban(connection: &mut SqliteConnection, node_id: &str) {
-    let user = users::unban(connection, node_id).unwrap();
+pub fn user_unban(conn: &mut SqliteConnection, node_id: &str) {
+    let user = users::unban(conn, node_id).unwrap();
     println!("Unbanned user #{}, '{}'", user.id, user.node_id);
 }
 
-pub fn board_list(connection: &mut SqliteConnection) {
+pub fn board_list(conn: &mut SqliteConnection) {
     println!(
         "\
 # BBS boards
@@ -63,7 +63,7 @@ pub fn board_list(connection: &mut SqliteConnection) {
 | Created at          | Num | Name                           | Description |
 | ------------------- | --- | ------------------------------ | ----------- |"
     );
-    for board in boards::all(connection) {
+    for board in boards::all(conn) {
         println!(
             "| {} | {:3} | {:30} | {} |",
             formatted_useconds(board.created_at_us),
@@ -74,16 +74,16 @@ pub fn board_list(connection: &mut SqliteConnection) {
     }
 }
 
-pub fn board_add(connection: &mut SqliteConnection, name: &str, description: &str) {
-    let board = boards::add(connection, name, description).unwrap();
+pub fn board_add(conn: &mut SqliteConnection, name: &str, description: &str) {
+    let board = boards::add(conn, name, description).unwrap();
     println!("Created board #{}, '{}'", board.id, board.name);
 }
 
-pub fn post_read(connection: &mut SqliteConnection, board_id: i32) {
-    let board = boards::get(connection, board_id).unwrap();
+pub fn post_read(conn: &mut SqliteConnection, board_id: i32) {
+    let board = boards::get(conn, board_id).unwrap();
     println!("# Posts in '{}'\n", board.name);
 
-    let post_info = posts::in_board(connection, board_id);
+    let post_info = posts::in_board(conn, board_id);
     if post_info.is_empty() {
         println!("There are no posts in board #{}", board_id);
         return;
@@ -105,8 +105,8 @@ pub fn post_read(connection: &mut SqliteConnection, board_id: i32) {
     }
 }
 
-pub fn post_add(connection: &mut SqliteConnection, board_id: i32, node_id: &str, content: &str) {
-    let user = users::get(connection, node_id).unwrap();
-    let post = posts::add(connection, user.id, board_id, content).unwrap();
+pub fn post_add(conn: &mut SqliteConnection, board_id: i32, node_id: &str, content: &str) {
+    let user = users::get(conn, node_id).unwrap();
+    let post = posts::add(conn, user.id, board_id, content).unwrap();
     println!("Created post #{}", post.id);
 }
