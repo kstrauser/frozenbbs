b740c00d3290f24c03ba20d2c32da6c086250b62
Kirk Strauser <kirk@strauser.com>
2024-12-08 10:41:15 -0800

    Read from MQTT instead of directly from the radio

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@ pub mod client;
 pub mod commands;
 pub mod db;
 pub mod server;
+pub mod server_mqtt;
+use serde::{Deserialize, Serialize};
 
-pub fn node_id_from_hex(node_id: &str) -> u32 {
+pub fn hex_id_to_num(node_id: &str) -> u32 {
     u32::from_str_radix(
         if node_id.starts_with("!") {
             node_id.strip_prefix("!").unwrap()
@@ -14,4 +16,38 @@ pub fn node_id_from_hex(node_id: &str) -> u32 {
         16,
     )
     .unwrap()
+}
+
+fn num_id_to_hex(node_num: u32) -> String {
+    format!("!{:x}", node_num)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BBSConfig {
+    my_id: String,
+    mqtt_root: String,
+    // Identifies this program to the MQTT broker
+    mqtt_id: String,
+    mqtt_hostname: String,
+    mqtt_port: u16,
+    mqtt_username: String,
+    mqtt_password: String,
+    meshtastic_python_path: String,
+    meshtastic_python_host: String,
+}
+
+impl ::std::default::Default for BBSConfig {
+    fn default() -> Self {
+        Self {
+            my_id: "cafeb33d".into(),
+            mqtt_root: "msh".into(),
+            mqtt_id: "frozenbbs".into(),
+            mqtt_hostname: "localhost".into(),
+            mqtt_port: 1883,
+            mqtt_username: "meshdev".into(),
+            mqtt_password: "large4cats".into(),
+            meshtastic_python_path: "./meshtastic-python".into(),
+            meshtastic_python_host: "localhost".into(),
+        }
+    }
 }
