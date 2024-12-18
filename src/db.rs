f43eda500048b5d5b25e3998f641bba3baf25cfd
Kirk Strauser <kirk@strauser.com>
2024-12-17 19:35:04 -0800

    Store the database URL in the config file

--- src/db.rs
+++ src/db.rs
@@ -6,19 +6,16 @@ use chrono::{Local, TimeZone, Utc};
 pub use models::{Board, Post, User};
 mod models;
 mod schema;
+use crate::BBSConfig;
 use diesel::connection::SimpleConnection;
 use diesel::prelude::*;
-use dotenvy::dotenv;
-use std::env;
 use validator::ValidationErrors;
 
 pub type Result<T> = std::result::Result<T, ValidationErrors>;
 
-pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let mut connection = SqliteConnection::establish(&database_url)
+pub fn establish_connection(cfg: &BBSConfig) -> SqliteConnection {
+    let database_url = &cfg.db_path;
+    let mut connection = SqliteConnection::establish(database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
     connection
         .batch_execute("PRAGMA foreign_keys = ON")
